9dc576b1bcc797f75875a7bc21157b3834e39e00
MSI\2210171 <2210171@ecc.ac.jp>
2022-05-17 16:19:55 +0900

    title

--- OpenSiv3D_0.6.31_BlindFPS/Title.h
+++ OpenSiv3D_0.6.31_BlindFPS/Title.h
@@ -8,6 +8,16 @@ public:
 	void update()override;
 	void draw() const override;
 private:
+	const Image image{ 1920, 1080, Color{ 0, 0, 0 } };
+	const Texture texture{ image };
+	const ColorF backgroundColor = ColorF{ 0, 0, 0 };
+	Texture titleName{ U"Image/TITLE.png" };
+	Texture START{ U"Image/START.png" };
+	Texture mouse{ U"Image/mouse.png" };
+	Texture exit{ U"Image/EXIT.png" };
+protected:
+	int menu = 0;
+	int  option = 0;
 	
 };
 
